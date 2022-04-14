ebb3280960b72058c949be2ae7e072adb83d6b70
gibstyle <gibsont@msoe.edu>
2022-04-14 12:10:29 -0500

    starter code for player move

--- player.cpp
+++ player.cpp
@@ -1,12 +1,50 @@
 #include "player.h"
 #include "board.h"
+#include "string"
 
 Player::Player(Board * board) {
-	// TODO
+	// set x and y position from board
+	isDead = false;
 }
 
 void Player::move(char direction) {
-	// TOOD
+	char move = tolower(direction);
+	if (move == 'n')
+	{
+		if (y - 1 >= 0)
+		{
+			// check what is in (x, y - 1) spot
+			y -= 1;
+		}
+	}
+	else if (move == 'e')
+	{
+		if(x + 1 <= 5)
+		{
+			// check what is in (x + 1, y) spot
+			x += 1;
+		}
+	}
+	else if (move == 's')
+	{
+		if (y + 1 <= 5)
+		{
+			// check what is in (x, y + 1)
+			y += 1;
+		}
+	}
+	else if (move == 'w')
+	{
+		if (x - 1 >= 0)
+		{
+			// check what is in (x - 1, y)
+			x -= 1;
+		}
+	}
+	else
+	{
+		// invalid move display
+	}
 }
 
 void Player::shoot() {
