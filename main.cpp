86accf9112551d969d599d3733da50faa7dd7526
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-04-21 09:41:46 -0500

    Fixed stuff

--- main.cpp
+++ main.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 string how_to_play() {
-	return "How to Play:\n" +
-		   "\tCommands:\n" +
-		   "\t\t- n/N = move north/up\n" +
-		   "\t\t- e/E = move east/right\n" + 
-		   "\t\t- s/S = move south/down\n" + 
-		   "\t\t- w/W = move west/left\n" +
-		   "\t\t- d/D = debug mode\n" + 
-		   "\t\t- m/M = view map\n" + 
-		   "\t\t- a/A = shoot arrow\n" + 
-		   "\t\t- h/H = pull up help menu\n\n" + 
-		   "\tMap Key:\n" +
-		   "\t\t- . = empty\n" +
-		   "\t\t- - = arrow\n" + 
-		   "\t\t- P = player\n" + 
-		   "\t\t- B = bats\n" +
-		   "\t\t- @ = hazard\n" + 
-		   "\t\t- ? = treasure\n" + 
-		   "\t\t- ! = monster\n\n" + 
+	return "How to Play:\n"
+		   "\tCommands:\n"
+		   "\t\t- n/N = move north/up\n"
+		   "\t\t- e/E = move east/right\n" 
+		   "\t\t- s/S = move south/down\n" 
+		   "\t\t- w/W = move west/left\n"
+		   "\t\t- d/D = debug mode\n" 
+		   "\t\t- m/M = view map\n" 
+		   "\t\t- a/A = shoot arrow\n" 
+		   "\t\t- h/H = pull up help menu\n\n" 
+		   "\tMap Key:\n"
+		   "\t\t- . = empty\n"
+		   "\t\t- - = arrow\n" 
+		   "\t\t- P = player\n" 
+		   "\t\t- B = bats\n"
+		   "\t\t- @ = hazard\n" 
+		   "\t\t- ? = treasure\n" 
+		   "\t\t- ! = monster\n\n" 
 		   "Press X to exit help menu";
 }
 
@@ -65,6 +65,7 @@ bool performAction(char action){
 	} else if (action == 'q'){
 		return false;
 	}
+	return ret;
 }
 
 int main() {
