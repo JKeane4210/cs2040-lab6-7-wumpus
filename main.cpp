b3c6e4338e1e0e84263437094bd5bc389f0f727e
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-04-21 09:32:37 -0500

    Added how to play

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <cstdio> 
+#include <string>
 
 using namespace std;
+
+string how_to_play() {
+	return "How to Play:\n" +
+		   "\tCommands:\n" +
+		   "\t\t- n/N = move north/up\n" +
+		   "\t\t- e/E = move east/right\n" + 
+		   "\t\t- s/S = move south/down\n" + 
+		   "\t\t- w/W = move west/left\n" +
+		   "\t\t- d/D = debug mode\n" + 
+		   "\t\t- m/M = view map\n" + 
+		   "\t\t- a/A = shoot arrow\n" + 
+		   "\t\t- h/H = pull up help menu\n";
+}
+
 
 int main() {
 	return 0;
