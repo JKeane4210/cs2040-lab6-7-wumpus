5412a28ebae0cbb5ec1b6ec174eebca530b7f010
goebela <goebela@msoe.edu>
2022-04-22 10:48:42 -0500

    Fixed the bat glitch

--- bats.cpp
+++ bats.cpp
@@ -6,9 +6,9 @@
 Bats::Bats(Board * board, int x, int y, int delta_x, int delta_y) : Hazard(board, x, y), delta_x{delta_x}, delta_y{delta_y} {}
 
 bool Bats::attack(Player * player) {
-	player->setLocation(delta_x, delta_x);
+	bool b = player->setLocation(delta_x, delta_y);
 	std::cout << "You were carried away by bats" << std::endl;
-	return true;
+	return b;
 }
 
 char Bats::display() {
