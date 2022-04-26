411493b61583f2b575a920d0f614d5999172f4f0
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-04-26 10:27:56 -0500

    Commented hazard inheritance hierarchy

--- bats.h
+++ bats.h
@@ -6,12 +6,21 @@
 
 class Board;
 
+// A swarm of bats that can carry a player away
 class Bats : public Hazard {
 	int delta_x, delta_y;
 public:
+	// Initializes a bats object on the board at coordinate (x, y) 
+	// and will move the player to (delta_x, delta_y)
 	Bats(Board * board, int x, int y, int delta_x, int delta_y);
+
+	// The bats attack the player by moving them to (delta_x, delta_y)
 	bool attack(Player * player);
+
+	// A 'B' symbol representing the bats on the map
 	char display();
+
+	// Message for when bats are close
 	std::string message();
 };
 
