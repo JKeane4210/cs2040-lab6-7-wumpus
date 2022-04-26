a226fa9d8f6c2999ee3bb7e18f8cf0910091a6dc
gibstyle <gibsont@msoe.edu>
2022-04-26 10:11:38 -0500

    comments for player classes

--- player.h
+++ player.h
@@ -9,17 +9,28 @@ class Player {
 	int arrows;
 	bool isDead;
 	Board * board;
+	// check to see if current postion is legal and if it is occupied
 	bool checkCurrentPosition(int x, int y);
+	// checks neighbors to see if hazards are nearby
 	void checkNeighbors(int x, int y);
+	// notify user if the Wumpus was hit
 	bool hitWumpus();
+	// notify user if the Wumpus was not hit
 	void missWumpus();
+	// notify user if shot is out of bounds
 	void shootOutOfBounds();
+	// if player attempts to move out of bounds, notify them
 	void moveOutOfBounds();
 public:
+	// default constructor for Player
 	Player(Board * board);
+	// moves player in specific direction
 	bool move(char direction);
+	// sets the new location of the player
 	bool setLocation(int x, int y);
+	// shoots an arrow in a given direction
 	bool shoot(char direction);
+	// get current number of arrows
 	int getArrows();
 };
 
