1a88fd3524581ae98f843a853deb0c56e0eb91cf
JKeane4210 <64436387+JKeane4210@users.noreply.github.com>
2022-04-26 10:40:09 -0500

    Fixed something really bad

--- bats.cpp
+++ bats.cpp
@@ -6,8 +6,8 @@
 Bats::Bats(Board * board, int x, int y, int delta_x, int delta_y) : Hazard(board, x, y), delta_x{delta_x}, delta_y{delta_y} {}
 
 bool Bats::attack(Player * player) {
-	bool b = player->setLocation(delta_x, delta_y);
 	std::cout << "You were carried away by bats" << std::endl;
+	bool b = player->setLocation(delta_x, delta_y);
 	return b;
 }
 
